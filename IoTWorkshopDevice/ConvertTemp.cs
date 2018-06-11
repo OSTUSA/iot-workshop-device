2cac063723b9fd232272f78589afbd3072128f9f
Brian Krygsman <bkrygsman@ostusa.com>
2018-06-10 21:41:13 -0400

    Add temperature units conversion class

---
+++ IoTWorkshopDevice/ConvertTemp.cs
@@ -0,0 +1,18 @@
+﻿namespace IoTWorkshopDevice
+{
+    /// <summary>
+    /// Temperature units conversion class
+    /// </summary>
+    public static class ConvertTemp
+    {
+        public static double ConvertCelsiusToFahrenheit( double c )
+        {
+            return ((9.0 / 5.0) * c) + 32;
+        }
+
+        public static double ConvertFahrenheitToCelsius( double f )
+        {
+            return (5.0 / 9.0) * (f - 32);
+        }
+    }
+}
